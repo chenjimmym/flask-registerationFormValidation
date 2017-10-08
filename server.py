892ab50e9272ec321ad241fdeba2b112dab9844d
Jimmy <chen.jimmym@gmail.com>
2017-10-08 16:50:33 -0700

    done

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, session, flash
+import re
 app = Flask(__name__)
 app.secret_key = 'aSecret'
+emailREGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 @app.route('/')
 def formPage():
@@ -8,7 +10,33 @@ def formPage():
 
 @app.route('/submit', methods=['POST'])
 def submitted():
-    print "success"
-    return redirect('/')
+    email = request.form['email']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    password = request.form['password']
+    passwordC = request.form['passwordConfirm']
+    state = True
+    if len(email) < 1 or len(firstName) < 1 or len(lastName) < 1 or len(password) < 1 or len(passwordC) < 1:
+        flash("All field must be filled")
+        return redirect('/')
+    if password != passwordC:
+        flash("Password must match")
+        state = False
+    if len(password) < 9:
+        flash("Password must be longer than 8 characters")
+        state = False
+    if not firstName.isalpha() or not lastName.isalpha():
+        flash("First and last name must be all alphabets")
+        state = False
+    if not emailREGEX.match(email):
+        flash("Invalid Email Address!")
+        state = False
+    if state == True:
+        flash("Successfully Submitted")
+        print "Success"
+        return redirect('/')
+    else:
+        print "Not Success"
+        return redirect('/')
 
 app.run(debug=True)
